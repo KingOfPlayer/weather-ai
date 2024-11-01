a82f5f68c0511fef6e0826dcc85e42c9349fe1e9
Erkan Hazır <kristalerkan_123@hotmail.com>
2024-11-01 13:55:59 +0300

    First Selenium Codes, Table Row example

--- Selenium/Crawl/Crawl.py
+++ Selenium/Crawl/Crawl.py
@@ -1,9 +1,8 @@
-from telnetlib import DO
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("https://www.wunderground.com/history/daily/tr/seyhan/LTAF/date/2017-12-28")
 
 driver.refresh()
@@ -15,8 +14,10 @@ while True:
         driver.refresh();
     if m == "g":
         ## mat-table cdk-table mat-sort ng-star-inserted
-        table = driver.find_element(By.CLASS_NAME,"mat-table cdk-table mat-sort ng-star-inserted")
-        print(table.text)
+        table = driver.find_elements(By.XPATH, "//table[@class='mat-table cdk-table mat-sort ng-star-inserted']/tbody[@role='rowgroup']/tr")
+        for row in table:
+            cols = row.find_elements(By.XPATH, "//span[@class='wu-value wu-value-to'][1]")
+            for col in cols:
+                print(col.text)
         
-
 driver.close()
